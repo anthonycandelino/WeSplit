74cbbb13c6c5ae272a11dd09d8aebdf50820d460
Anthony Candelino <anthcandelino@gmail.com>
2024-06-08 11:28:22 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Anthony Candelino on 2024-06-08.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
